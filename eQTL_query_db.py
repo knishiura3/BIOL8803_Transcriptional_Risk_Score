b015491298d6d788760248047c3f1a28906b75ca
Kenji Nishiura <kenji@gatech.edu>
2022-10-25 21:05:52 -0400

    removing redundant code

--- eQTL_query_db.py
+++ eQTL_query_db.py
@@ -44,10 +44,10 @@ class eqtl_DB:
         return self.cursor.fetchall()
 
 
-#@click.command()
-#@click.argument("input_gwas_dir", type=click.Path(exists=True), default="gwas_hits")
-#@click.argument("db_name", type=click.Path(exists=True), default="eQTLs.db")
-#@click.argument("output_dir", type=click.Path(), default="output")
+# @click.command()
+# @click.argument("input_gwas_dir", type=click.Path(exists=True), default="gwas_hits")
+# @click.argument("db_name", type=click.Path(exists=True), default="eQTLs.db")
+# @click.argument("output_dir", type=click.Path(), default="output")
 def main(input_file, db_name, output_dir):
 
     manager = eqtl_DB(db_name)
@@ -100,16 +100,14 @@ def main(input_file, db_name, output_dir):
                 # id_gwas = str(segs[10])
 
                 # capture time info for logging
-                current_date = (
-                    f"{datetime.datetime.now().strftime('%Y-%m-%d')}"
+                current_date = f"{datetime.datetime.now().strftime('%Y-%m-%d')}"
+                current_date_and_time = (
+                    f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
                 )
-                current_date_and_time = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
 
                 # query the eQTL DB for eQTLs in the exact position of GWAS row
                 try:
-                    results = [
-                        manager.query_position(chr_gwas, pos_gwas)[0]
-                    ]
+                    results = [manager.query_position(chr_gwas, pos_gwas)[0]]
                 except IndexError:
                     print(f"no eQTLs found for {chr_gwas}:{pos_gwas}")
                     continue
@@ -187,119 +185,9 @@ def main(input_file, db_name, output_dir):
                 ]
                 # append df["Pvalue"] to output list without a header
                 output_list.append(df.values.tolist()[0])
-                # output_list.append(
-                #     df.to_csv(header=False, index=False, line_terminator="")
-                # )
-                # get rid of the lines below once this is working
-                continue
-                # create an output directory if it doesn't exist already
-                if not os.path.exists(f"{output_dir}"):
-                    os.makedirs(f"{output_dir}")
-                # write eQTLs to tab-delimited file in output directory
-                with open(
-                    f"{output_dir}/{pval_rank_gwashit_str}_{chr_gwas_tophit}_{lower_pos_gwas_tophit}_{upper_pos_gwas_tophit}_eQTLs.tsv",
-                    "a",
-                ) as out:
-                    # checks if header has been written to file yet
-                    if header_check is None:
-                        header = f"pvalues\tN\tMAF\tbeta\tvarbeta\ttype\tsnp\tz\tchr\tpos\tid\n"
-                        out.write(header)
-                        header_check = True
-                    # total_eQTL_count = 0
-                    SNP_set = set()
-                    output_list = []
-                    # looping over eQTL DB query results
-                    for result in results:
-                        # assign result to variables
-                        Pvalue = float(result[0])
-                        # discard eQTL rows where p-value = 1
-                        if Pvalue == 1:
-                            continue
-                        SNP = str(result[1])
-                        SNPChr = int(result[2])
-                        SNPPos = int(result[3])
-                        # AssessedAllele = str(result[4])
-                        # OtherAllele = str(result[5])
-                        Zscore = float(result[6])
-                        # Gene = str(result[7])
-                        # GeneSymbol = str(result[8])
-                        # GeneChr = int(result[9])
-                        # GenePos = int(result[10])
-                        # NrCohorts = int(result[11])
-                        NrSamples = int(result[12])
-                        # FDR = float(result[13])
-                        # BonferroniP = float(result[14])
 
-                        # calculated variables
-                        # source:  Zhu, Z. et al. Integration of summary data from GWAS and eQTL studies predicts complex trait gene targets.
-                        # betaSE <- 1/sqrt(2*p*(1-p)*(n+z^2))
-                        # beta <- z/sqrt(2*p*(1-p)*(n+z^2))
-                        # try calculating betaSE and if there's an error, print the values of Pvalue, NrSamples, and Zscore
-                        try:
-                            betaSE = 1 / sqrt(
-                                2
-                                * Pvalue
-                                * (1 - Pvalue)
-                                * (NrSamples + Zscore**2)
-                            )
-                            beta = Zscore / sqrt(
-                                2
-                                * Pvalue
-                                * (1 - Pvalue)
-                                * (NrSamples + Zscore**2)
-                            )
-                        except:
-                            print(
-                                f"Error calculating beta/betaSE:  Pvalue={Pvalue} NrSamples={NrSamples} Zscore={Zscore}"
-                            )
-                        # set MAF to eaf from gwas hits, if not defined, set to 0.5
-                        MAF_eqtl = MAF_gwas if MAF_gwas != "NA" else 0.5
-
-                        # variance of beta is simply standard error squared
-                        varbeta = betaSE**2
-
-                        type = "quant"
-
-                        # since SQL query results are already sorted by SNP and p-value,
-                        # we only need to keep the first row for each unique SNP, add to list, sort list by SNP,
-                        # and then loop through list to write to file
-
-                        if SNP not in SNP_set:
-                            SNP_set.add(SNP)
-                            output_list.append(
-                                [
-                                    Pvalue,
-                                    NrSamples,
-                                    MAF_eqtl,
-                                    beta,
-                                    varbeta,
-                                    type,
-                                    SNP,
-                                    Zscore,
-                                    SNPChr,
-                                    SNPPos,
-                                    db_name.split(".")[0],
-                                    "\n",
-                                ]
-                            )
-
-                            total_eQTL_count += 1
-                        else:
-                            continue
-
-                        # export raw eQTL rows
-                        # out.write(
-                        #     f"{result[0]}\t{result[1]}\t{result[2]}\t{result[3]}\t{result[4]}\t{result[5]}\t{result[6]}\t{result[7]}\t{result[8]}\t{result[9]}\t{result[10]}\t{result[11]}\t{result[12]}\t{result[13]}\t{result[14]}\n"
-                        # )
-
-                    # list comprehension to write tab-separated, terminating each line w/ newline
-                    out.write(
-                        "\n".join(
-                            "\t".join(map(str, row)) for row in output_list
-                        )
-                    )
             # increment progressbar counter
-            #bar()
+            # bar()
             # write header and output_list to file
             with open(
                 f"{output_dir}/{pval_rank_gwashit_str}_{chr_gwas_tophit}_{lower_pos_gwas_tophit}_{upper_pos_gwas_tophit}_eQTLs.tsv",
@@ -319,11 +207,7 @@ def main(input_file, db_name, output_dir):
                     "db_name",
                 ]
                 out.write("\t".join(header) + "\n")
-                out.write(
-                    "\n".join(
-                        "\t".join(map(str, row)) for row in output_list
-                    )
-                )
+                out.write("\n".join("\t".join(map(str, row)) for row in output_list))
             # write to log
             total_eQTL_count = len(output_list)
             with open(
@@ -343,9 +227,9 @@ if __name__ == "__main__":
     # @click.argument("db_name", type=click.Path(exists=True), default="eQTLs.db")
     # @click.argument("output_dir", type=click.Path(), default="output")
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', type=str, required=True)
-    parser.add_argument('--db_name', type=str, required=True)
-    parser.add_argument('--output_dir', type=str, required=True)
+    parser.add_argument("--input_file", type=str, required=True)
+    parser.add_argument("--db_name", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, required=True)
     args = parser.parse_args()
     main(args.input_file, args.db_name, args.output_dir)
 
