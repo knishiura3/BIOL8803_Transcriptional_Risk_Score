4f6a855a914157889df9c39a244a83e7616f6a58
Colin Naughton <Naughtoncolin@gmail.com>
2022-12-13 17:47:05 -0500

    REF: Change database location to data. Should've done in previous commit

--- MAF_build_parquet.R
+++ MAF_build_parquet.R
@@ -1,12 +1,12 @@
 library(arrow)
 
-dir_output <- "db/eqtl_MAF"
+dir_output <- "data/eqtl_MAF"
 
 # eqtl data loaded from https://molgenis26.gcc.rug.nl/downloads/eqtlgen/cis-eqtl/2019-12-11-cis-eQTLsFDR-ProbeLevel-CohortInfoRemoved-BonferroniAdded.txt.gz and unzipped
 # load eqtl data
-ds_maf <- open_dataset("db/eqtl_MAF/2018-07-18_SNP_AF_for_AlleleB_combined_allele_counts_and_MAF_pos_added.txt", format = "tsv")
+ds_maf <- open_dataset("data/eqtl_MAF/2018-07-18_SNP_AF_for_AlleleB_combined_allele_counts_and_MAF_pos_added.txt", format = "tsv")
 
 # creates a separate parquet file for each chromosome
 arrow::write_dataset(ds_maf, dir_output, partitioning = "hg19_chr", format = "parquet")
 
-file.remove("db/eqtl_MAF/2018-07-18_SNP_AF_for_AlleleB_combined_allele_counts_and_MAF_pos_added.txt")
+file.remove("data/eqtl_MAF/2018-07-18_SNP_AF_for_AlleleB_combined_allele_counts_and_MAF_pos_added.txt")
